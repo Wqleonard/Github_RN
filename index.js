44af3d2481dfe93cc33d6c0f4f4d551b7adf2ba5
wangqian <wangqian@mz-cloud.com>
2018-12-12 19:21:40 +0800

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import AppNavigator from './js/navigator/AppNavigator';
+// import WelcomePage from './js/page/WelcomePage';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => AppNavigator);
